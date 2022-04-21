aca8dd2f6efa83b2d3161771a7469e7a952fd45e
Rajat Jain <rajat.apple007@gmail.com>
2022-04-21 00:38:52 -0700

    MongoDB connection

--- services/backend/MongoBackend.js
+++ services/backend/MongoBackend.js
@@ -1,15 +1,26 @@
 /* eslint-disable no-useless-constructor */
 /* eslint-disable class-methods-use-this */
 /* eslint-disable no-empty-function */
-const CoinAPI = require('../CoinAPI');
+const { MongoClient } = require("mongodb");
+const CoinAPI = require("../CoinAPI");
 
 class MongoBackend {
-
   constructor() {
     this.coinAPI = new CoinAPI();
+    this.monogoUrl = "mongodb://localhost:37017";
+    this.client = null;
+    this.collection = null;
   }
 
-  async connect() {}
+  async connect() {
+    const mongoClient = new MongoClient(this.monogoUrl, {
+      useUnifiedTopology: true,
+      useNewUrlParser: true,
+    });
+    this.client = await mongoClient.connect();
+    this.collection = this.client.db("bitcoinpriceindex").collection("values");
+    return this.client;
+  }
 
   async disconnect() {}
 
@@ -17,7 +28,17 @@ class MongoBackend {
 
   async getMax() {}
 
-  async max() {}
+  async max() {
+    console.log("Connecting to MongoDB...");
+    console.time("mongoDB-connect");
+    const client = await this.connect();
+    if (client.isConnected()) {
+      console.info("MongoDB connected...");
+    } else {
+      throw new Error("MongoDB connect failed");
+    }
+    console.timeEnd("mongoDB-connect");
+  }
 }
 
 module.exports = MongoBackend;
